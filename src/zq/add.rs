da01be8b7372c3fa75ba1e3555f247f8081554df
0xLucqs <lucaslevy10@gmail.com>
2025-08-01 14:53:53 +0200

    WIP2

--- src/zq/add.rs
+++ src/zq/add.rs
@@ -156,7 +156,9 @@ impl FrameworkEval for Eval {
 
         // Constraint: a + b = quotient * Q + remainder
         eval.add_constraint(
-            a + b.clone() - quotient * E::F::from(M31::from_u32_unchecked(Q)) - remainder.clone(),
+            a.clone() + b.clone()
+                - quotient * E::F::from(M31::from_u32_unchecked(Q))
+                - remainder.clone(),
         );
         // Now we need to check that the remainder is in the range [0, Q)
         // We do this by using the range check we defined. Here we increment the multiplicity of
@@ -172,7 +174,12 @@ impl FrameworkEval for Eval {
             E::EF::one(),
             &[b],
         ));
-        eval.finalize_logup_in_pairs();
+        eval.add_to_relation(RelationEntry::new(
+            &self.lookup_elements,
+            E::EF::one(),
+            &[a],
+        ));
+        eval.finalize_logup();
         eval
     }
 }
@@ -237,6 +244,20 @@ impl InteractionClaim {
         for vec_row in 0..(1 << (log_size - LOG_N_LANES)) {
             // Get the remainder value from the trace (column 3)
             let result_packed = trace[1].data[vec_row];
+
+            // Create the denominator using the lookup elements
+            let denom: PackedQM31 = lookup_elements.combine(&[result_packed]);
+
+            // The numerator is 1 (we want to check that remainder is in the range)
+            let numerator = PackedQM31::one();
+
+            col_gen.write_frac(vec_row, numerator, denom);
+        }
+        col_gen.finalize_col();
+        let mut col_gen = logup_gen.new_col();
+        for vec_row in 0..(1 << (log_size - LOG_N_LANES)) {
+            // Get the remainder value from the trace (column 3)
+            let result_packed = trace[0].data[vec_row];
 
             // Create the denominator using the lookup elements
             let denom: PackedQM31 = lookup_elements.combine(&[result_packed]);
